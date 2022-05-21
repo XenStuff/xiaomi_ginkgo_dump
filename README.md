6731a566eab71c48194c38154195339e4a1d61ef
xenxynon <flynryder427@gmail.com>
2022-05-21 04:28:11 +0000

    Add extras for ginkgo-user 11 RKQ1.201004.002 V12.5.4.0.RCOCNXM release-keys
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## ginkgo-user 11 RKQ1.201004.002 V12.5.4.0.RCOCNXM release-keys
+- Manufacturer: xiaomi
+- Platform: trinket
+- Codename: ginkgo
+- Brand: xiaomi
+- Flavor: ginkgo-user
+- Release Version: 11
+- Id: RKQ1.201004.002
+- Incremental: V12.5.4.0.RCOCNXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 440
+- Fingerprint: xiaomi/ginkgo/ginkgo:11/RKQ1.201004.002/V12.5.4.0.RCOCNXM:user/release-keys
+- OTA version: 
+- Branch: ginkgo-user-11-RKQ1.201004.002-V12.5.4.0.RCOCNXM-release-keys
+- Repo: xiaomi_ginkgo_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null >> system/system/data-app/MiShop/MiShop.apk
+rm -f system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null
+cat system/system/data-app/VipAccount/VipAccount.apk.* 2>/dev/null >> system/system/data-app/VipAccount/VipAccount.apk
+rm -f system/system/data-app/VipAccount/VipAccount.apk.* 2>/dev/null
+cat system/system/data-app/MiFinance/MiFinance.apk.* 2>/dev/null >> system/system/data-app/MiFinance/MiFinance.apk
+rm -f system/system/data-app/MiFinance/MiFinance.apk.* 2>/dev/null
+cat system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null >> system/system/data-app/Youpin/Youpin.apk
+rm -f system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null
+cat system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f system/system/product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/app/Music/Music.apk.* 2>/dev/null >> system/system/app/Music/Music.apk
+rm -f system/system/app/Music/Music.apk.* 2>/dev/null
+cat system/system/app/Mipay/Mipay.apk.* 2>/dev/null >> system/system/app/Mipay/Mipay.apk
+rm -f system/system/app/Mipay/Mipay.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk.* 2>/dev/null >> cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk
+rm -f cust/app/customized/partner-com.taobao.taobao_24/partner-com.taobao.taobao_24.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null >> cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk
+rm -f cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null >> cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk
+rm -f cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk.* 2>/dev/null >> cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk
+rm -f cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk.* 2>/dev/null
